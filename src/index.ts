9b789a81047d9c9eb0e3269bcb9f443be1481dff
mikohan <angara99@gmail.com>
2020-09-26 17:48:28 +0300

    Implemented events system

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('hi therer');
+import { User } from './models/User';
+
+const user = new User({ name: 'Vlad', age: 49 });
+
+user.on('change', () => {
+  console.log('Change one');
+});
+
+user.on('change', () => {
+  console.log('Change two');
+});
+
+user.on('some_custom_event', () => {
+  console.log('Change custom_evnt');
+});
+
+user.trigger('change');
